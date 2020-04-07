a07cfac2d909dcbcc63483a20ba5dacee8057190
Luigi Ferrettino <s254300@studenti.polito.it>
2020-04-07 23:45:38 +0200

    main windows fixed at the center of the screen at run

--- client/startwindow.cpp
+++ client/startwindow.cpp
@@ -19,7 +19,7 @@
 StartWindow::StartWindow(QWidget *parent): QMainWindow(parent, Qt::FramelessWindowHint | Qt::WindowSystemMenuHint),
     ui(new Ui::StartWindow)
 {
-    QRect screenGeometry = QApplication::desktop()->screenGeometry();
+    /*QRect screenGeometry = QApplication::desktop()->screenGeometry();
     double width = screenGeometry.width();
     int minWidth = 1920;
     double scale = width / minWidth;
@@ -27,7 +27,29 @@ StartWindow::StartWindow(QWidget *parent): QMainWindow(parent, Qt::FramelessWind
     QByteArray scaleAsQByteArray(scaleAsString.c_str(), scaleAsString.length());
     qputenv("QT_SCALE_FACTOR", scaleAsQByteArray);
 
-    ui->setupUi(this);  //IS AN HALF HELP WITH THE DPI-Related-BUG - DON'T DELETE ME FOR NOW
+    ui->setupUi(this);  //IS AN HALF HELP WITH THE DPI-Related-BUG - DON'T DELETE ME FOR NOW*/
+    QDesktopWidget *desktop = QApplication::desktop();
+
+    int screenWidth, width;
+    int screenHeight, height;
+    int x, y;
+    QSize windowSize;
+
+    screenWidth = desktop->width(); // get width of screen
+    screenHeight = desktop->height(); // get height of screen
+
+    windowSize = size(); // size of our application window
+    width = windowSize.width();
+    height = windowSize.height();
+
+    // little computations
+    x = (screenWidth - width) / 2;
+    y = (screenHeight - height) / 2;
+    y -= 50;
+
+    // move window to desired coordinates
+    move ( x, y );
+    ui->setupUi(this);
 }
 
 //DESTRUCTOR
