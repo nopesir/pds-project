f764efade9e3c20e6b20aa319f5a663921b0c0c6
Luigi Ferrettino <s254300@studenti.polito.it>
2020-08-14 15:16:48 +0200

    added build and run script

---
+++ server/build/CMakeFiles/ServerModule.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerModule.dir/Client.cpp.o"
+  "CMakeFiles/ServerModule.dir/File.cpp.o"
+  "CMakeFiles/ServerModule.dir/Message.cpp.o"
+  "CMakeFiles/ServerModule.dir/Server.cpp.o"
+  "CMakeFiles/ServerModule.dir/Session.cpp.o"
+  "CMakeFiles/ServerModule.dir/SharedEditor.cpp.o"
+  "CMakeFiles/ServerModule.dir/Symbol.cpp.o"
+  "CMakeFiles/ServerModule.dir/SymbolStyle.cpp.o"
+  "CMakeFiles/ServerModule.dir/dbService.cpp.o"
+  "CMakeFiles/ServerModule.dir/email.cpp.o"
+  "CMakeFiles/ServerModule.dir/fileUtility.cpp.o"
+  "CMakeFiles/ServerModule.dir/jsonUtility.cpp.o"
+  "CMakeFiles/ServerModule.dir/main.cpp.o"
+  "ServerModule"
+  "ServerModule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerModule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
